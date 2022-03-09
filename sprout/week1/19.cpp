8f9c55aec47a31e190765d137d33aa0359743820
W-X-Dai <s910466@gapp.ylsh.ilc.edu.tw>
2022-03-09 22:15:27 +0800

    3/9,ubuntu,unfinished

--- sprout/week1/19.cpp
+++ sprout/week1/19.cpp
@@ -2,8 +2,9 @@
 #define AC ios::sync_with_stdio(0),cin.tie(0);
 using namespace std;
 
-int T,N,arr[100005];
+int T,N,arr[100005],x;
 stack<int> st;
+queue<int> q;
 
 int main(){
 //  AC    
@@ -11,28 +12,14 @@ int main(){
     cin >>T;
     while(T--){
         while(!st.empty())st.pop();
-        
+        while(!q.empty())q.pop();
         cin >>N;
         for(int i=1;i<=N;++i){
-            cin >>arr[i];
+            cin >>x;q.push(x);
         }
         int cnt=0,j=1;
-        for(int i=1;i<=N;){
-            int now=arr[i];
-            if(j==now){
-            //    cout<<"j"<<'\n';
-            //    cout<<j<<" ";
-                ++cnt,++j;++i;
-            }else if(!st.empty()){
-                cout<<st.top();
-                while(!st.empty() && st.top()==now){
-                    cout<<st.top()<<" "<<st.size();
-                    st.pop();++cnt;++i;
-                }
-            }else{
-                cout<<"jk"<<j<<'\n';
-                st.push(j),++j;++i;
-            }
+        for(int i=1;i<=N;++i){
+            if(i==q.front());
         }
         cout<<"cnt:"<<cnt<<'\n';
         if(cnt==N)cout<<"Yes\n";
