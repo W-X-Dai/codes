b2c6017d2946910c7829e233307ab8df6fc91a8b
W-X-Dai <s910466@gapp.ylsh.ilc.edu.tw>
2022-03-09 09:32:58 +0800

    3/9,unfinished

--- sprout/week1/19.cpp
+++ sprout/week1/19.cpp
@@ -13,11 +13,30 @@ int main(){
         while(!st.empty())st.pop();
         
         cin >>N;
-        for(int i=0;i<N;++i){
+        for(int i=1;i<=N;++i){
             cin >>arr[i];
         }
-
-        
+        int cnt=0,j=1;
+        for(int i=1;i<=N;){
+            int now=arr[i];
+            if(j==now){
+            //    cout<<"j"<<'\n';
+            //    cout<<j<<" ";
+                ++cnt,++j;++i;
+            }else if(!st.empty()){
+                cout<<st.top();
+                while(!st.empty() && st.top()==now){
+                    cout<<st.top()<<" "<<st.size();
+                    st.pop();++cnt;++i;
+                }
+            }else{
+                cout<<"jk"<<j<<'\n';
+                st.push(j),++j;++i;
+            }
+        }
+        cout<<"cnt:"<<cnt<<'\n';
+        if(cnt==N)cout<<"Yes\n";
+        else cout<<"No\n";
     }
 
 }
