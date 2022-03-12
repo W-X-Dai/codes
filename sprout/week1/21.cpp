98e3c222528caf7629ef91d220b4ea7c4d0c34ee
W-X-Dai <s910466@gapp.ylsh.ilc.edu.tw>
2022-03-12 14:59:47 +0800

    3/12,ubuntu

---
+++ sprout/week1/21.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[100005],n,m,mod,x;
+
+int main(){
+    cin >>n>>m;
+    for(int i=1;i<=n;++i)arr[i]=i;
+    for(int i=0;i<m;++i){
+        cin >>mod>>x;
+    }
+}
