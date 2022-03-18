a586a5f9e3c0de112195c0437fec98ae932d463a
W-X-Dai <s910466@gapp.ylsh.ilc.edu.tw>
2022-03-18 23:06:23 +0800

    evil things,3/18,ubuntu

---
+++ evil/IO壓常數.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int in(){
+    int rt=0,f=1;
+    char ch=getchar();
+    while(ch<'0' && ch>'9'){
+        if(ch=='-')f=-1;
+        ch=getchar();
+    }
+    while(ch>='0' && ch<='9'){
+        rt=rt*10+ch-48;
+        ch=getchar();
+    }
+    return rt*f;
+
+}
+
+void out(int x){
+    if(x==0){putchar('0');return;}
+    int len=0,num[20];
+
+    while(x>0){
+        num[++len]=x%10;
+        x/=10;
+    }
+    for(;len>=1;--len){
+        putchar(num[len]+48);
+    }
+}
+
+int main(){
+    int x=in();
+    out(x-10);
+}
