b275cb0694c00fd4be461e2dbf7adfe26b206a06
W-X-Dai <s910466@gapp.ylsh.ilc.edu.tw>
2022-04-08 22:35:32 +0800

    4/8,new ubutnu

---
+++ 林燈的課程/a+b_problem/testdata.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+mt19937 rd(seed); 
+uniform_int_distribution<long long> dist(2147483645, 2147483648),dis(1,9999); 
+
+
+int main(){
+    ll a,b;
+    ofstream f1("a.in",ios::trunc),f2("a.out",ios::trunc);
+
+    int n=100000;f1<<n<<'\n';
+    for(int i=0;i<n;++i){
+        a=dist(rd),b=dist(rd);
+        f1<<a<<" "<<b<<'\n';
+        f2<<a+b<<'\n';
+    }
+}
