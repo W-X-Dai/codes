e824acabcb78a9d113391e372fdf23bca2b9041a
W-X-Dai <s910466@gapp.ylsh.ilc.edu.tw>
2022-04-17 01:29:23 +0800

    4/17,windows

--- evil/autojudge/log.cpp
+++ evil/autojudge/log.cpp
@@ -15,18 +15,18 @@ std::string timeToString(std::chrono::system_clock::time_point &t){
 }
 
 
-inline void excuse(int n,int t){
+inline void execuse(int n,int t){
     string s="0_0000.in";
     char data[10];
     for(int i=0;i<10;++i)data[i]=s[i];
     data[0]=n+'0';
-    
+    begi=clock();
     if(t<10)data[5]=t+'0';
     else data[4]='1';
-
-    freopen(data,"r",stdin);
+	for(int i=0;i<1000000;++i);
+	freopen(data,"r",stdin);
     freopen("test.out","w",stdout);
-    begi=clock();
+    
     sol();
     endt=clock();
 }
@@ -37,14 +37,15 @@ int main(){
     
     ofstream f("log.txt",ios::trunc);
     
-    f<<"The project was excuted on "<<timeToString(time_p)<<'\n';
+    cerr<<"The project was executed on "<<timeToString(time_p)<<'\n';
 
     int cnt=0;
     for(int j=1;j<=4;++j)
     for(int i=0;i<=10;++i){
         f<<"testdata #"<<cnt<<" ";++cnt;
-        excuse(j,i);
-        f<<"excute time:"<<(double)(endt-begi)/CLOCKS_PER_SEC<<'\n';
+    //  excuse(j,i);
+	//	double difftime(begi,endt);
+        f<<"execute time:"<<(double)(endt-begi)/CLOCKS_PER_SEC<<'\n';
     }
     
 }
---
+++ sprout/week7/140.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define mod  1000007
+#define ll long long
+using namespace std;
+
+ll n,x,dp[100005][3];
+//0:R 1:G 2:B
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0);
+    dp[1][0]=1,dp[1][1]=1,dp[1][2]=1;
+
+    for(int i=1;i<100001;++i){
+        dp[i][0]=
+    }
+
+    cin >>n;
+    for(int i=0;i<n;++i){
+        cin >>x;
+        cout<<(dp[x][0]%mod+dp[x][1]%mod+dp[x][2]%mod)%mod<<'\n';
+    }
+}
