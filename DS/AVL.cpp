ea1b3f4bf5f07a76d7bd74ec0f1fb716cb33e16c
josh <joshdai930908@gmail.com>
2025-05-27 18:57:26 +0800

    avl tree

---
+++ DS/AVL.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class AVLtree{
+    struct Node{
+        int data;
+        Node *left;
+        Node *right;
+        int height;
+    };
+
+
+};
+
+int main(){
+
+}
